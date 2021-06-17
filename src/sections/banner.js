030565db85544ae159e730dbb575e42c5a371592
Marcos-Mutuma7 <maccosmutuma1981@gmail.com>
2021-06-17 08:00:01 +0300

    initial

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -11,7 +11,7 @@ export default function Banner() {
       <Container sx={styles.banner.container}>
         <Box sx={styles.banner.contentBox}>
           <Heading as="h1" variant="heroPrimary">
-            Macc designs for your startup business.
+            Top quality designs to explore.
           </Heading>
           <Text as="p" variant="heroSecondary">
             Get your blood tests delivered at let home collect sample from the
